7e141cb290b70db9774cd627b37ee0c4407a3390
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2022-05-05 09:32:51 -0300

    Corrected test solutions

--- algo/tests/Parcial1/src/Exercicio4.java
+++ algo/tests/Parcial1/src/Exercicio4.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercicio4 {
     public static void main(String[] args) {
         String nome;
-        int anoNasc, anosServiço;
+        int anoNasc, anoServiço;
 
         Scanner kb = new Scanner(System.in);
 
@@ -13,12 +13,12 @@ public class Exercicio4 {
         System.out.print("Digite o ano de nascimento de " + nome + ": ");
         anoNasc = kb.nextInt();
 
-        System.out.print("Digite quantos anos de seriço tem " + nome + ": ");
-        anosServiço = kb.nextInt();
+        System.out.print("Digite quantos ano de ingresso de " + nome + ": ");
+        anoServiço = kb.nextInt();
 
         kb.close();
 
-        if (2022 - anoNasc >= 65 || anosServiço >= 30 || (2022 - anoNasc >= 60 && anosServiço >= 25)) {
+        if (2022 - anoNasc >= 65 || 2022 - anoServiço >= 30 || (2022 - anoNasc >= 60 && 2022 - anoServiço >= 25)) {
             System.out.println("Requerer aposentadoria");
         } else {
             System.out.println("Não requerer");
