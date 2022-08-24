2f229bff9ee36c9d01f2575d9ed8467f4a014a8d
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2022-08-24 10:54:31 -0300

    Corrected exercises

--- pl-sql/revisao.sql
+++ pl-sql/revisao.sql
@@ -12,9 +12,9 @@ select
 	concat(c.primeiro_nome, ' ', c.ultimo_nome) as nome_cliente, 
     f.titulo as nome_filme_alugado
 from aluguel a
+inner join cliente c on c.cliente_id = a.cliente_id
 inner join inventario i on i.inventario_id = a.inventario_id
-inner join filme f on f.filme_id = i.filme_id
-inner join cliente c on c.cliente_id = a.cliente_id;
+inner join filme f on f.filme_id = i.filme_id;
 
 -- 3. Selecione todos os pagamentos de todos os clientes. Traga nos resultados: o nome do cliente e o pagamento correspondente.
 
@@ -101,12 +101,11 @@ group by l.loja_id;
 select 
 	l.loja_id as cod_loja,
 	cast(a.data_de_aluguel as date) as data_aluguel, 
-    count(fi.filme_id) as qtd_filme
+    count(i.inventario_id) as qtd_filme
 from aluguel a
 inner join funcionario f on a.funcionario_id = f.funcionario_id
 inner join loja l on l.loja_id = f.loja_id
 inner join inventario i on i.inventario_id = a.inventario_id
-inner join filme fi on fi.filme_id = i.filme_id
 group by cod_loja, data_aluguel;
 
 -- 12. Selecione o nome completo dos clientes e suas cidades de origem.
@@ -121,23 +120,25 @@ inner join cidade ci on ci.cidade_id = e.cidade_id;
 -- 13. Descubra a quantidade de cada filme no inventario de cada loja.
 
 select 
+	i.loja_id as id_loja,
 	f.titulo as nome_filme, 
     count(i.inventario_id) as qtd_inventario
 from inventario i
 inner join filme f on f.filme_id = i.filme_id
-group by f.filme_id;
+group by i.loja_id, f.filme_id;
 
 -- 14. Selecione todas as categorias de filmes que a atriz MARY TANDY não atua.
 
-select
-    c.nome as nome_categoria
+select c.nome
 from categoria c
-inner join filme_categoria fc on c.categoria_id = fc.categoria_id
-inner join filme f on f.filme_id = fc.filme_id
-inner join filme_ator fa on fa.filme_id = f.filme_id
-inner join ator a on fa.ator_id = a.ator_id
-where not(a.primeiro_nome = 'MARY' and a.ultimo_nome = 'TANDY')
-group by nome_categoria;
+where c.categoria_id not in (
+	select distinct c.categoria_id
+	from ator a
+	inner join filme_ator fa on fa.ator_id = a.ator_id
+	inner join filme_categoria fc on fc.filme_id = fa.filme_id
+	inner join categoria c on c.categoria_id = fc.categoria_id
+	where a.primeiro_nome = 'MARY' and a.ultimo_nome = 'TANDY'
+);
 
 -- 15. Descubra o faturamento e o ticket médio por dia (exiba apenas uma casa decimal), busque apenas os dias em que o faturamento for maior que 300.
 
